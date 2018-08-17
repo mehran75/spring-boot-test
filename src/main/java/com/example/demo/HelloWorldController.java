862d18850df29efd9f276d80fcaf3a4c76f5adbd
mehran <mehranrafiee5@gmail.com>
2018-08-17 18:32:47 +0430

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+}
